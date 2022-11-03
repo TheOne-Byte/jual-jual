9fede1b2ba4950a0aed092ec509355837414ac58
richard483 <richard.william483@gmail.com>
2022-11-03 22:16:14 +0700

    feat: added ItemDetail page and controller

--- resources/views/TemplateLayoutHomePage/Main.blade.php
+++ resources/views/TemplateLayoutHomePage/Main.blade.php
@@ -1,134 +1,154 @@
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <title>HomePage</title>
-  </head>
-  <body>
+</head>
+
+<body>
 
     @include('Navbars.NavBar');
 
-      <div class="container mt-3">
+    <div class="container mt-3">
         @yield('container')
-      </div>
+    </div>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+</body>
+
 </html>
 
 
 <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
     <ol class="carousel-indicators">
-      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
     </ol>
     <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img class="d-block w-100" src="HomePagePNG/AkunWR.jpg" alt="First slide">
-      </div>
-      <div class="carousel-item">
-        <img class="d-block w-100" src="HomePagePNG/AkunML.jpg" alt="Second slide">
-      </div>
-      <div class="carousel-item">
-        <img class="d-block w-100" src="HomePagePNG/AkunFF.jpg" alt="Third slide">
-      </div>
+        <div class="carousel-item active">
+            <img class="d-block w-100" src="HomePagePNG/AkunWR.jpg" alt="First slide">
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="HomePagePNG/AkunML.jpg" alt="Second slide">
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="HomePagePNG/AkunFF.jpg" alt="Third slide">
+        </div>
     </div>
     <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
     </a>
     <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="sr-only">Next</span>
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
     </a>
-  </div>
+</div>
 
-  <!-- Footer --> <!-- Footer --><!-- Footer --><!-- Footer --><!-- Footer --><!-- Footer --><!-- Footer --><!-- Footer --><!-- Footer --><!-- Footer --><!-- Footer -->
-  <!-- Footer -->
+<!-- Footer -->
+<!-- Footer -->
+<!-- Footer -->
+<!-- Footer -->
+<!-- Footer -->
+<!-- Footer -->
+<!-- Footer -->
+<!-- Footer -->
+<!-- Footer -->
+<!-- Footer -->
+<!-- Footer -->
+<!-- Footer -->
 <footer class="bg-light text-center ">
     <!-- Grid container -->
     <div class="container p-4">
 
-      <!-- Section: Social media -->
-      <section class="mb-4">
-        <!-- Facebook -->
-        <img src="HomePagePNG/FacebookLogo.jpg" alt="FacebookLogo">
+        <!-- Section: Social media -->
+        <section class="mb-4">
+            <!-- Facebook -->
+            <img src="HomePagePNG/FacebookLogo.jpg" alt="FacebookLogo">
 
-        <!-- Twitter -->
-        <img src="HomePagePNG/Twitter.png" alt="TwitterLogo">
+            <!-- Twitter -->
+            <img src="HomePagePNG/Twitter.png" alt="TwitterLogo">
 
-        <!-- Instagram -->
-        <img src="HomePagePNG/InstagramLogo.jpg" alt="InstagramLogo">
+            <!-- Instagram -->
+            <img src="HomePagePNG/InstagramLogo.jpg" alt="InstagramLogo">
 
-        <!-- Linkedin -->
-        <img src="HomePagePNG/LinkedinLogo.jpg" alt="LinkedInLogo">
-     </section>
-      <!-- Section: Social media -->
+            <!-- Linkedin -->
+            <img src="HomePagePNG/LinkedinLogo.jpg" alt="LinkedInLogo">
+        </section>
+        <!-- Section: Social media -->
 
- <!-- Section: Links -->
- <section class="">
-    <!--Grid row-->
-    <div class="row">
-      <!--Grid column-->
-      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-        <h5 class="text-uppercase">Facebook</h5>
-
-
-      </div>
-      <!--Grid column-->
-
-      <!--Grid column-->
-      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-        <h5 class="text-uppercase">Twitter</h5>
-      </div>
-      <!--Grid column-->
-
-      <!--Grid column-->
-      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-        <h5 class="text-uppercase">Instagram</h5>
-      </div>
-      <!--Grid column-->
-
-      <!--Grid column-->
-      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-        <h5 class="text-uppercase">LinkedIn</h5>
-      </div>
-      <!--Grid column-->
-    </div>
-    <!--Grid row-->
-  </section>
+        <!-- Section: Links -->
+        <section class="">
+            <!--Grid row-->
+            <div class="row">
+                <!--Grid column-->
+                <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                    <h5 class="text-uppercase">Facebook</h5>
 
 
-      <!-- Section: Text -->
-      <section class="mb-4">
-        <p>
-            Our website will help you and sell all kinds of account that you are looking for
-        </p>
-      </section>
+                </div>
+                <!--Grid column-->
+
+                <!--Grid column-->
+                <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                    <h5 class="text-uppercase">Twitter</h5>
+                </div>
+                <!--Grid column-->
+
+                <!--Grid column-->
+                <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                    <h5 class="text-uppercase">Instagram</h5>
+                </div>
+                <!--Grid column-->
+
+                <!--Grid column-->
+                <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                    <h5 class="text-uppercase">LinkedIn</h5>
+                </div>
+                <!--Grid column-->
+            </div>
+            <!--Grid row-->
+        </section>
 
 
-      <!-- Section: Links -->
+        <!-- Section: Text -->
+        <section class="mb-4">
+            <p>
+                Our website will help you and sell all kinds of account that you are looking for
+            </p>
+        </section>
+
+
+        <!-- Section: Links -->
 
     </div>
     <!-- Grid container -->
 
     <!-- Copyright -->
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
-      © 2020 Copyright Jual-Jual
-      {{-- <a class="text-dark" href="https://mdbootstrap.com/">MDBootstrap.com</a> --}}
+        © 2020 Copyright Jual-Jual
+        {{-- <a class="text-dark" href="https://mdbootstrap.com/">MDBootstrap.com</a> --}}
     </div>
     <!-- Copyright -->
 
-  </footer>
-  <!-- Footer -->
+</footer>
+<!-- Footer -->
